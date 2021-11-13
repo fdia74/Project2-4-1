98f6d3b65b4671a3a5cb524c286680359b6b1447
fdia74 <fdia@mail.ru>
2021-11-13 15:24:57 +0300

    initial commit

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -5,11 +5,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class SQRServiceTest {
 
     @org.junit.jupiter.api.Test
-    void shouldCalculateNumberSQR() {
+    void shouldCalculateNumberSQR1() {
         SQRService sqrService = new SQRService();
 
         int expected = 3;
         int actual = sqrService.calculateSQR(200,300);
+        assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateNumberSQR2() {
+        SQRService sqrService = new SQRService();
+
+        int expected = 0;
+        int actual = sqrService.calculateSQR(-200,-300);
+        assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateNumberSQR3() {
+        SQRService sqrService = new SQRService();
+
+        int expected = 8;
+        int actual = sqrService.calculateSQR(0,300);
         assertEquals(expected, actual);
     }
 }
